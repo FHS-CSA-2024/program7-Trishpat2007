ee87143ee30093f8b8bc30768a30c14fb1c9966b
Trishpat2007 <trishpat2007@gmail.com>
2024-08-28 09:06:12 -0700

    Update Program.java
    not final commit, need to do calcs

--- Program.java
+++ Program.java
@@ -1,7 +1,27 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
-
+class Program7{
+    public static void main (String[] args){
+        //make scanner
+        Scanner myScanner = new Scanner(System.in);
+        //Create Variabels-nd
+        int schruteBucks=0;
+        int kelvins=0;
+        int stanleyNickel=0;
+        //Ask for input and store
+        System.out.println("Enter schrute-bucks: ");
+        schruteBucks= myScanner.nextInt();
+        System.out.println("Enter Kelvins: ");
+        kelvins= myScanner.nextInt();
+        System.out.println("Enter stanley-nickels: ");
+        stanleyNickel= myScanner.nextInt();
+        //Do Calculations-nd
+        
+        //Create Output-nd
+        System.out.print("Decimal schrute-bucks:");
+    }
+}
 //Paste console output below:
 /*
 
